0b76c07a05a807847688ade25eacae1bb1190825
getmanovanton <getmanov11@mail.ru>
2019-10-21 12:49:23 +0300

    commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper('.swiper-container', {
+    cssMode: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+});
+var mobileMenu = document.getElementById('mobile-menu');
+document.getElementById('burger-js').onclick = function () {
+    this.classList.toggle('burger-on');
+    mobileMenu.classList.toggle('mobile-menu-on')
+};
+
+
